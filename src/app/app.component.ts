0b104055f50413b16a52d4b97f46b8ffc864031c
Steven Sell <steven.sell@cscglobal.com>
2018-09-07 20:51:27 -0400

    Worked on Landing Page & Altering App Component on Router Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { AuthService } from './core/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { DataSharingService } from './services/data-sharing.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DataSharingService]
 })
 export class AppComponent {
   title = 'app';
-
+  isLandingPage: boolean;
   router;
   constructor(private auth: AuthService, private _router: Router) {
     this.router = _router.url;
-    // console.log(this.router);
+
+    _router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd === true && _router.url !== '/') {
+        this.isLandingPage = false;
+      } else if (val instanceof NavigationEnd === true && _router.url === '/') {
+        this.isLandingPage = true;
+      }
+    });
   }
 }
