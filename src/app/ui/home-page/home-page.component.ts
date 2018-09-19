5a6572af4918fd4285588c6fcccd0f80db89924b
Steven Sell <steven.sell@cscglobal.com>
2018-09-19 17:25:03 -0400

    Verified Database Connectivity

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { firebase } from '@firebase/app';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {
+/*interface Invitee {
+  name: string;
+  ref: string;
+}*/
+
+export class HomePageComponent {
+  invitees: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) {
+    this.invitees = db.collection('invitees').valueChanges();
   }
-
 }
